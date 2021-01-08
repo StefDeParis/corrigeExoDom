d079929b0b0fb0320d2f3ea3d9edfb7da09ef5ba
Stef <stefdeparis@live.fr>
2021-01-08 10:02:16 +0100

    add exo1

---
+++ assets/js/file.js
@@ -0,0 +1,14 @@
+//Q1
+// function changer_style() {
+//     var p = document.getElementById('parag1');
+//     p.style.color = "white";
+//     p.style.backgroundColor = "#bbb";
+//     p.style.border = "1px solid gray";
+//     p.style.padding = "5px";
+// }
+
+//Q2
+function changer_style() { 
+    var p=document.getElementById('parag1');
+    p.classList.add("active");    
+}
